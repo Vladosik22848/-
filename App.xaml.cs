3143621991f518d10663510da7120db5355894bc
Vladosik22848 <obokovvladik@mail.ru>
2025-11-15 16:10:16 +0300

    Add files via upload

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,9 +8,7 @@ using System.Windows;
 
 namespace Kursovaya
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+
     public partial class App : Application
     {
     }
